dc6951847d6e3a519aff34b164cf9d994d6bb176
Patrick <s2302175@usls.edu.ph>
2026-08-14 11:32:48 +0800

    Revert "Add run_task_safely function (Deliberate Mistake)"
    This reverts commit 5f605a7346643a032dab5b0ce6f7df2872d444c6.

--- src/project/main.py
+++ src/project/main.py
@@ -77,10 +77,6 @@ def fleet_report(robots):
         except InsufficientBatteryError as error:
             print(f"[{robot.name} FAILED] {error}")
 
-def run_task_safetly(robot):
-    print(f"Running {robot.name} unsafely...")
-    robot.perform_task()
-
 
 # Test fleet_report
 if __name__ == "__main__":
